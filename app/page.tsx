c5997fff68f4604f1f489e5f72e3c64517a9ff1f
RybaLP <148885360+RybaLP@users.noreply.github.com>
2026-01-16 09:07:39 +0100

    feat(ui): changed css class in component

--- app/page.tsx
+++ app/page.tsx
@@ -21,16 +21,16 @@ export default function Home() {
         <div className="relative z-10 flex flex-col sm:flex-row gap-6 mt-8">
           
           <Link href="/login" 
-            className="px-12 py-4 bg-linear-to-b from-blue-500 to-blue-700 text-white font-bold rounded-md border-b-4 border-blue-900 hover:brightness-125 hover:scale-105 active:border-b-0 active:translate-y-1 transition-all duration-200 uppercase tracking-wider min-w-[200px] text-center shadow-lg shadow-blue-900/20">
+            className="px-12 py-4 bg-linear-to-b from-blue-500 to-blue-700 text-white font-bold rounded-md border-b-4 border-blue-900 hover:brightness-125 hover:scale-105 active:border-b-0 active:translate-y-1 transition-all duration-200 uppercase tracking-wider min-w-50 text-center shadow-lg shadow-blue-900/20">
             Log In
           </Link>
 
           <Link href="/register" 
-            className="px-12 py-4 bg-linear-to-b from-amber-500 to-amber-700 text-white font-bold rounded-md border-b-4 border-amber-900 hover:brightness-125 hover:scale-105 active:border-b-0 active:translate-y-1 transition-all duration-200 uppercase tracking-wider min-w-[200px] text-center shadow-lg shadow-amber-900/20">
+            className="px-12 py-4 bg-linear-to-b from-amber-500 to-amber-700 text-white font-bold rounded-md border-b-4 border-amber-900 hover:brightness-125 hover:scale-105 active:border-b-0 active:translate-y-1 transition-all duration-200 uppercase tracking-wider min-w-50 text-center shadow-lg shadow-amber-900/20">
             Register
           </Link>
-          
+
         </div>
     </main>
   );
-} 
+}
