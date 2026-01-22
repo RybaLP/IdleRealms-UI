4359673c7d029522b6f6cb5413813765be2d3bfc
RybaLP <148885360+RybaLP@users.noreply.github.com>
2026-01-22 23:25:46 +0100

    feat : implement hero slice - state store for fetched hero

---
+++ app/store/heroSlice.ts
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { HeroProfile } from '../types/hero/hero';
+import { heroService } from '../services/heroService';
+
+
+export const fetchHeroData = createAsyncThunk(
+  'hero/fetchData',
+  async (_, { rejectWithValue }) => {
+    try {
+      return await heroService.getMyHero()
+    } catch (error: any) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+interface HeroState {
+  data: HeroProfile | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: HeroState = {
+  data: null,
+  loading: false,
+  error: null,
+};
+
+const heroSlice = createSlice({
+  name: 'hero',
+  initialState,
+  reducers: {
+    logoutHero: (state) => {
+      state.data = null;
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchHeroData.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchHeroData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchHeroData.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
+  },
+});
+
+export const { logoutHero } = heroSlice.actions;
+export default heroSlice.reducer;
