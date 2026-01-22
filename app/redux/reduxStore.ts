ee76ca201e5869950e5aa05cda6935d7c95de9c7
RybaLP <148885360+RybaLP@users.noreply.github.com>
2026-01-22 23:22:30 +0100

    feat : extended configuration - setup useAppDispatch and useAppSelector

--- app/redux/reduxStore.ts
+++ app/redux/reduxStore.ts
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { registerSlice } from '../store/registerSlice';
+import heroSlice from '../store/heroSlice';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
     register: registerSlice.reducer, 
+    hero : heroSlice
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
