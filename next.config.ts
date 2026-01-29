8bcaf83e1ea00ddf94c74a0037656b60d66d689e
RybaLP <148885360+RybaLP@users.noreply.github.com>
2026-01-29 22:28:28 +0100

    feat : next config update and added new image to public folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', 
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
